a9dfc1f8cb47f3cab34cd56ff05f8c107b9e94a4
Sungjoon Park <sungjoon512@gmail.com>
2021-06-22 23:24:21 +0900

    import modern-css-reset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import { AppProps } from 'next/app'
+import 'modern-css-reset'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
+
+export default MyApp
