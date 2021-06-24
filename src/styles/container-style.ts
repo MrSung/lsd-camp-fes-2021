c25dfafe107c02ee6f90eb1ae0ff569be3527bfa
Sungjoon Park <sungjoon512@gmail.com>
2021-06-24 22:42:59 +0900

    set min width

--- src/styles/container-style.ts
+++ src/styles/container-style.ts
@@ -3,13 +3,14 @@ import { css } from 'styled-components'
 import { Style } from '@/const/style'
 
 export const containerStyle = css`
+  max-width: 1170px;
   margin-right: auto;
   margin-left: auto;
   padding-right: 16px;
   padding-left: 16px;
 
   @media (min-width: ${Style.BREAKPOINT.MD}) {
-    max-width: 1170px;
+    min-width: 1170px;
     padding-right: 24px;
     padding-left: 24px;
   }
