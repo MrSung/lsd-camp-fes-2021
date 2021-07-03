4caafb7f2b0f47a5e392e4333731853f9abd9b3f
Sungjoon Park <sungjoon512@gmail.com>
2021-07-04 01:10:37 +0900

    add more func to date utils

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,13 @@
+const locales = `ja-JP`
+const option: Intl.DateTimeFormatOptions = {
+  timeZone: `Asia/Tokyo`,
+}
+
 export const dateToJaStdDateTime = (dateStr: string) =>
-  new Date(dateStr).toLocaleString(`ja-JP`, {
-    timeZone: `Asia/Tokyo`,
-  })
+  new Date(dateStr).toLocaleString(locales, option)
+
+export const dateToJaStdTime = (dateStr: string) =>
+  new Date(dateStr).toLocaleTimeString(locales, option)
 
 export const dateToJaStdDate = (dateStr: string) =>
-  new Date(dateStr).toLocaleDateString(`ja-JP`, {
-    timeZone: `Asia/Tokyo`,
-  })
+  new Date(dateStr).toLocaleDateString(locales, option)
