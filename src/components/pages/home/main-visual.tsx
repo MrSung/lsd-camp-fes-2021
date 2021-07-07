affe99b2ff449848661e0bbf3c6ce24a46bdffd1
Sungjoon Park <sungjoon512@gmail.com>
2021-07-07 23:13:03 +0900

    add fade-in animation to main visual images

--- src/components/pages/home/main-visual.tsx
+++ src/components/pages/home/main-visual.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import styled from 'styled-components'
+import { motion as Motion } from 'framer-motion'
 
 import titleMessage from '@/images/title-message.svg'
 import mvIllustration from '@/images/mv-illustration.png'
@@ -7,21 +8,38 @@ import { Style } from '@/const/style'
 import { sectionStyle, containerStyle } from '@/styles'
 import { SITE_TITLE, MAIN_MESSAGE } from './meta'
 
+const ImageContainer: React.FC = ({ children }) => (
+  <Motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{
+      duration: 1,
+      ease: `easeInOut`,
+    }}
+  >
+    {children}
+  </Motion.div>
+)
+
 export const MainVisual = () => (
   <Section>
     <Container>
       <ColLeft>
         <h1>
-          <Image
-            src={titleMessage}
-            alt={`${SITE_TITLE} ${MAIN_MESSAGE}`}
-            width={470}
-            height={168}
-          />
+          <ImageContainer>
+            <Image
+              src={titleMessage}
+              alt={`${SITE_TITLE} ${MAIN_MESSAGE}`}
+              width={470}
+              height={168}
+            />
+          </ImageContainer>
         </h1>
       </ColLeft>
       <ColRight>
-        <Image src={mvIllustration} alt="" width={569} height={661} />
+        <ImageContainer>
+          <Image src={mvIllustration} alt="" width={569} height={661} />
+        </ImageContainer>
       </ColRight>
     </Container>
   </Section>
