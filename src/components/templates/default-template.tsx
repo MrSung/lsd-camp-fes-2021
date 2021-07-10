cd0a1696ef2bb9d747f7c15afc6b6cea066a7f5c
Sungjoon Park <sungjoon512@gmail.com>
2021-07-10 16:39:40 +0900

    add icon and heading to footer

--- src/components/templates/default-template.tsx
+++ src/components/templates/default-template.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import Image from 'next/image'
 import styled from 'styled-components'
 import { Link } from 'react-scroll'
 
+import favicon from '@/images/favicon.png'
 import { HOME_LINKS } from '@/const/home-links'
 import { CREDIT_LIST } from '@/const/credit-list'
 import { Style } from '@/const/style'
 import { GlobalStyle, containerStyle } from '@/styles'
+import { SITE_TITLE } from '@/components/pages/home/meta'
 import { ExternalLink } from '@/components/parts'
 
 interface IDefaultTemplateProps {
@@ -30,8 +32,9 @@ export const DefaultTemplate = ({ children }: IDefaultTemplateProps) => (
     </Header>
     <Main>{children}</Main>
     <Footer>
-      <Container>
-        <ul>
+      <FooterContainer>
+        <FooterHeading>{SITE_TITLE}</FooterHeading>
+        <FooterList>
           {CREDIT_LIST.map((o) => (
             <li key={o.name}>
               {o.work} by
@@ -46,8 +49,11 @@ export const DefaultTemplate = ({ children }: IDefaultTemplateProps) => (
               </ExternalLink>
             </li>
           ))}
-        </ul>
-      </Container>
+        </FooterList>
+        <FooterImageContainer>
+          <Image src={favicon} alt="" width={200} height={200} />
+        </FooterImageContainer>
+      </FooterContainer>
     </Footer>
   </>
 )
@@ -100,12 +106,49 @@ const Footer = styled.footer`
   padding: 72px 0;
   background-color: ${Style.COLOR.DARK_STATE_GRAY};
   color: ${Style.COLOR.WHITISH_GRAY};
+
+  @media (min-width: ${Style.BREAKPOINT.MD}px) {
+    min-width: ${Style.SIZE.CONTAINER_WIDTH}px;
+  }
 `
 
-const Container = styled.div`
+const FooterContainer = styled.div`
   ${containerStyle}
 
   @media (min-width: ${Style.BREAKPOINT.MD}px) {
+    display: grid;
+    grid-template-columns: 1fr 200px;
     font-size: 18px;
+  }
+`
+
+const FooterHeading = styled.h2`
+  font-size: 24px;
+
+  @media (min-width: ${Style.BREAKPOINT.MD}px) {
+    grid-column: 1 / 2;
+    grid-row: 1 / 2;
+  }
+`
+
+const FooterList = styled.ul`
+  @media (min-width: ${Style.BREAKPOINT.MD}px) {
+    grid-column: 1 / 2;
+    grid-row: 2 / 3;
+    margin-bottom: 0;
+  }
+`
+
+const FooterImageContainer = styled.div`
+  @media (max-width: ${Style.BREAKPOINT.MD - 1}px) {
+    width: 100px;
+    height: 100px;
+  }
+
+  @media (min-width: ${Style.BREAKPOINT.MD}px) {
+    display: flex;
+    align-items: center;
+    grid-column: 2 / 3;
+    grid-row: 1 / 3;
   }
 `
