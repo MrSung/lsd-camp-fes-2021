bfed2d23d8125cdc92ba8a8daf2616c93e00d683
Sungjoon Park <sungjoon512@gmail.com>
2021-07-18 09:16:07 +0900

    add program sorting

--- src/components/pages/home/program/index.tsx
+++ src/components/pages/home/program/index.tsx
@@ -1,3 +1,4 @@
+import { parse, isBefore, isAfter } from 'date-fns'
 import styled from 'styled-components'
 
 import { VenueKey } from '@/contents/venue'
@@ -18,20 +19,65 @@ export const Program = ({
   <Section id={sectionId}>
     <Container>
       <ProgramHeading>program</ProgramHeading>
-      {contents.map((c) => (
-        <ThumbLink
-          key={c.id}
-          href={c.link}
-          labelNo={c.venue[0] as VenueKey}
-          src={c.thumbnail.url as StaticImageData}
-          alt=""
-          width={270}
-          height={270}
-          time={`${c.eventDate[0]} ${c.startTime}〜${c.endTime}`}
-          title={c.title}
-          host={c.host}
-        />
-      ))}
+      {contents
+        .sort((a, b) => {
+          const aed = parse(a.eventDate[0], `MM/dd`, new Date())
+          const bed = parse(b.eventDate[0], `MM/dd`, new Date())
+
+          if (isBefore(aed, bed)) {
+            return -1
+          }
+          if (isAfter(aed, bed)) {
+            return 1
+          }
+          return 0
+        })
+        .sort((a, b) => {
+          const avk = Number(a.venue[0])
+          const bvk = Number(b.venue[0])
+
+          if (avk < bvk) {
+            return -1
+          }
+          if (avk > bvk) {
+            return 1
+          }
+          return 0
+        })
+        .sort((a, b) => {
+          const asd = parse(
+            `${a.eventDate[0]} ${a.startTime}`,
+            `MM/dd HH:mm`,
+            new Date(),
+          )
+          const bsd = parse(
+            `${b.eventDate[0]} ${b.startTime}`,
+            `MM/dd HH:mm`,
+            new Date(),
+          )
+
+          if (isBefore(asd, bsd)) {
+            return -1
+          }
+          if (isAfter(asd, bsd)) {
+            return 1
+          }
+          return 0
+        })
+        .map((c) => (
+          <ThumbLink
+            key={c.id}
+            href={c.link}
+            labelNo={c.venue[0] as VenueKey}
+            src={c.thumbnail.url as StaticImageData}
+            alt=""
+            width={270}
+            height={270}
+            time={`${c.eventDate[0]} ${c.startTime}〜${c.endTime}`}
+            title={c.title}
+            host={c.host}
+          />
+        ))}
     </Container>
   </Section>
 )
