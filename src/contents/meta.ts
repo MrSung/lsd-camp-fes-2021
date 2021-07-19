141a16f83428816be267ba87ecd5bb4c6d33b314
Sungjoon Park <sungjoon512@gmail.com>
2021-07-19 20:54:16 +0900

    Update meta description

--- src/contents/meta.ts
+++ src/contents/meta.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = `LSD.Camp Fes 2021`
-export const SITE_DESCRIPTION = `LSD.Camp Fes 2021 は ○○ なイベントです`
+export const SITE_DESCRIPTION = `人は誰もがアーティスト 〜未完成な自分から始めよう〜 をテーマに、Camper やその周りの人たちがアーティスト性を発揮し、つながりを生む LSD.Camp 5周年フェスの特設ページです。`
 export const SITE_URL = `https://lsd-camp-fes-2021.vercel.app/`
 export const MAIN_MESSAGE = `人は誰もがアーティスト`
